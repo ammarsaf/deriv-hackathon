9e9021711620ecb1488f0045c1ca9f2f05b264d8
Ammar-Azman <muhammarsafuan@gmail.com>
2025-02-08 22:47:26 +0800

    rename faiq to forge_docs, update ui

--- dispute_ui.py
+++ dispute_ui.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from openai import OpenAI
-from forge_docs_llm import llm_forge_docs
+from forge_docs import docs_forger_classifier
+from io import StringIO
+import json
+import ast
 
 SELLER_PROMPT_BEHAVIOR = """
     You are a seller from Deriv who want to sell 1 BTC for $1000. 
@@ -11,12 +14,22 @@ SELLER_PROMPT_BEHAVIOR = """
 
 
 def upload_pdf_sidebar():
+    if "forger" not in st.session_state:
+        st.session_state.forger = []
+
     with st.sidebar:
         pdf_uploaded = st.file_uploader("Upload Transaction File")
+
         if pdf_uploaded:
-            user_output = llm_forge_docs(pdf_uploaded)  # legit pdf
-            # scammer_output = llm_forge_docs("...")  # scammer pdf
-            return user_output
+            # stringio = StringIO(pdf_uploaded.getvalue().decode("utf-8"))
+
+            buyer_output = docs_forger_classifier(
+                pdf_uploaded.read(), role="buyer"
+            )  # legit pdf
+            # seller_output = llm_forge_docs("...")  # scammer pdf
+            # st.write(buyer_output)
+
+            st.session_state.forger.append(buyer_output)
 
 
 def seller_conversation():
@@ -34,6 +47,9 @@ def seller_conversation():
     # output state
     if "aioutput" not in st.session_state:
         st.session_state.aioutput = []
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = []
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -59,24 +75,19 @@ def seller_conversation():
             )
             response = st.write_stream(stream)
             st.session_state.aioutput.append(response)
+
+        st.session_state.conversation.append({"user": prompt, "system": response})
         st.session_state.messages.append({"role": "assistant", "content": response})
-        deviate_warning()
-
-    # st.write(st.session_state.messages)
-    # st.write(st.session_state.aioutput)
 
 
-def deviate_warning():
-    response: list = st.session_state.aioutput
-    if len(response) > 0:
-        # st.write(response)
-        for i in range(len(response)):
-            if "Whatsapp" in response[i] or "Telegram" in response[i]:
-                st.warning(
-                    "Warning: Seller try to deviate transaction from this platform!"
-                )
-            else:
-                return
+def deviate_warning(response):
+    # st.write(response)
+    # for i in range(len(response)):
+    if "whatsapp" in response.lower() or "telegram" in response.lower():
+
+        st.error("Warning: Seller try to deviate transaction from this platform!")
+    else:
+        return
 
 
 if __name__ == "__main__":
@@ -87,6 +98,8 @@ if __name__ == "__main__":
 
     with tab_chat:
         seller_conversation()
+        for res in st.session_state.aioutput:
+            deviate_warning(res)
 
     with tab_analysis:
         analyze_button = st.button("Analyze")
@@ -94,3 +107,18 @@ if __name__ == "__main__":
         st.title("Conversation Analysis")
 
         st.title("Document Forge Analysis")
+        if st.session_state.forger:
+            dict_out = st.session_state.forger[0]
+            json_out = json.loads(dict_out.replace("`", "").replace("json", ""))
+
+            st.header("Dispute verdict")
+            st.write(" ".join(json_out["dispute_verdict"].split("_")).title())
+
+            st.header("Analysis")
+            # for out in json_out["analysis"]:
+            # st.write(type(json_out["analysis"]))
+            for i in json_out["analysis"]:
+                st.write(i)
+
+                # st.write(dict_out["dispute_verdict"])
+                # st.write(dict_out["analysis"])
