4a37dc74373f94ec8d5a9ed7e2fd3e6b95b1facd
Ammar-Azman <muhammarsafuan@gmail.com>
2025-02-08 20:41:07 +0800

    add forge docs llm

--- dispute_ui.py
+++ dispute_ui.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from openai import OpenAI
+from forge_docs_llm import llm_forge_docs
 
 SELLER_PROMPT_BEHAVIOR = """
-    You are a seller from Deriv who want to sell 1 BTC for RM1000. 
+    You are a seller from Deriv who want to sell 1 BTC for $1000. 
     You try to deviate the conversation to Whatsapp, Telegram or other platform
     to continue the transaction process.
+    Please follow this system prompt. 
 """
 
 
 def upload_pdf_sidebar():
     with st.sidebar:
         pdf_uploaded = st.file_uploader("Upload Transaction File")
+        if pdf_uploaded:
+            user_output = llm_forge_docs(pdf_uploaded)  # legit pdf
+            # scammer_output = llm_forge_docs("...")  # scammer pdf
+            return user_output
 
 
 def seller_conversation():
@@ -19,11 +25,15 @@ def seller_conversation():
 
     # Set a default model
     if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
+        st.session_state["openai_model"] = "gpt-4o"
 
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
+
+    # output state
+    if "aioutput" not in st.session_state:
+        st.session_state.aioutput = []
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -48,11 +58,39 @@ def seller_conversation():
                 stream=True,
             )
             response = st.write_stream(stream)
+            st.session_state.aioutput.append(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
+        deviate_warning()
+
+    # st.write(st.session_state.messages)
+    # st.write(st.session_state.aioutput)
+
+
+def deviate_warning():
+    response: list = st.session_state.aioutput
+    if len(response) > 0:
+        # st.write(response)
+        for i in range(len(response)):
+            if "Whatsapp" in response[i] or "Telegram" in response[i]:
+                st.warning(
+                    "Warning: Seller try to deviate transaction from this platform!"
+                )
+            else:
+                return
 
 
 if __name__ == "__main__":
     st.title("AI Dispute Resolver")
+    tab_chat, tab_analysis = st.tabs(["Chat", "Analysis"])
 
     upload_pdf_sidebar()
-    seller_conversation()
+
+    with tab_chat:
+        seller_conversation()
+
+    with tab_analysis:
+        analyze_button = st.button("Analyze")
+
+        st.title("Conversation Analysis")
+
+        st.title("Document Forge Analysis")
